ad6f5187f66e370fae909cdfd0f7c64092ee847d
Jaehwa Noh <shwoghk14@gmail.com>
2020-03-23 16:43:33 +0900

    Start 3 version 0.3

--- Start 3/mnist_tensorflow.py
+++ Start 3/mnist_tensorflow.py
@@ -1,26 +1,21 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist
 
 (train_image, train_label), (test_image, test_label) = mnist.load_data()
 
-print(tf.reduce_max(train_image))
-print(tf.reduce_min(train_image))
+plt.figure(figsize=(25/2.54, 18/2.54))
+for i in range(25):
+    plt.subplot(5, 5, i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_image[i])
+    plt.xlabel(train_label[i])
 
+plt.show()
 
-
-
-
-# plt.figure(figsize=(25/2.54, 18/2.54))
-# for i in range(25):
-#     plt.subplot(5, 5, i+1)
-#     plt.xticks([])
-#     plt.yticks([])
-#     plt.grid(False)
-#     plt.imshow(train_image[i])
-#     plt.xlabel(train_label[i])
-#
-# plt.show()
 
 
 #
