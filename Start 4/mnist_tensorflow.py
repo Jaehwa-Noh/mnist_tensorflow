2c7d5ca837ccc34be99b1a68314fe687b4b55e22
Jaehwa Noh <shwoghk14@gmail.com>
2020-03-24 15:11:35 +0900

    Start 4 version 0.2

--- Start 4/mnist_tensorflow.py
+++ Start 4/mnist_tensorflow.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
+from mnist_util import split_train_validation
+
 mnist = tf.keras.datasets.mnist
 
 (train_image, train_label), (test_image, test_label) = mnist.load_data()
@@ -8,8 +10,12 @@ mnist = tf.keras.datasets.mnist
 train_image = (train_image > 10).astype(int)
 test_image = (test_image > 10).astype(int)
 
-print(len(train_image))
-print(len(test_image))
+
+train_image, train_label, validation_image, validation_label\
+= split_train_validation(train_image, train_label, 80)
+
+print(len(train_image), len(train_label))
+print(len(validation_image), len(validation_label))
 
 
 # plt.figure(figsize=(25/2.54, 18/2.54))
